c321ef8c696d3ab2b5b8ad2c8cbb5841ba39a028
jinsupark4255 <jin04007@naver.com>
2023-08-31 17:27:16 +0900

    네비게이션 바에 hover 및 클릭 이벤트 생성

--- kuiz/src/contents/jsfiles/main.js
+++ kuiz/src/contents/jsfiles/main.js
@@ -1,21 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import '../cssfiles/main.css';
 import { ReactComponent as Logo } from '../images/logo.svg';
 import { ReactComponent as User } from '../images/Ellipse 2.svg';
 import { ReactComponent as Logout } from '../images/logoutbutton.svg';
 
 const MainPage = () => {
+    const [activenav, setActivenav] = useState(null);
+
+    const handleClick = (index) => {
+        setActivenav(index);
+    }
     return (
         <div className="mainPage">
             <div className="header">
-                <Logo className='logo'/>
-                <div className="nav-detail">문제 생성</div>
-                <div className="nav-detail">PDF 업로드</div>
-                <div className="nav-detail">나의 시험지</div>
-                <div className="nav-detail">결제 정보</div>
+                <Logo className='logo' />
+                <div
+                    className={`nav-detail ${activenav === 0 ? "active" : ""}`}
+                    onClick={() => handleClick(0)}
+                >
+                    문제 생성
+                </div>
+                <div
+                    className={`nav-detail ${activenav === 1 ? "active" : ""}`}
+                    onClick={() => handleClick(1)}
+                >
+                    PDF 업로드
+                </div>
+                <div
+                    className={`nav-detail ${activenav === 2 ? "active" : ""}`}
+                    onClick={() => handleClick(2)}
+                >
+                    나의 시험지
+                </div>
+                <div
+                    className={`nav-detail ${activenav === 3 ? "active" : ""}`}
+                    onClick={() => handleClick(3)}
+                >
+                    결제 정보
+                </div>
                 <div></div>
-                <Logout className='logout'/>
-                <User className='userLogo'/>
+                <Logout className='logout' />
+                <User className='userLogo' />
             </div>
             <section>{/*왼쪽 PDF 섹션*/}
                 <div className="section_header"></div>
