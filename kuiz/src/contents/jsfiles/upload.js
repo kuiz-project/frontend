40ebde33e04875f220489557585fb4d45393d5eb
jinsupark4255 <jin04007@naver.com>
2023-09-01 16:14:34 +0900

    PDF 업로드 파트 css 반응형 및 중간화면 컴포넌트 50% 완성

--- kuiz/src/contents/jsfiles/upload.js
+++ kuiz/src/contents/jsfiles/upload.js
@@ -1,21 +1,31 @@
 import React, { useState } from "react";
 import { ReactComponent as Trash } from '../images/trash.svg';
 import { ReactComponent as Add } from '../images/add.svg';
-export const UploadView = () => {
+import { ReactComponent as Search } from '../images/search.svg';
 
+export const UploadView = () => {
     return (
-        <div className="main_view">
+        <div className="main_view2">
             <section>
                 <div className="section_header"><Trash className='trash_button' /><Add className='add_button' /></div>
                 <div className="section_lists"></div>
             </section>
             <div className="upload_mid_section">
-                <div className="upload_mid_top"></div>
+                <div className="upload_mid_top">
+                    <div className="upload_name">
+                        강의명
+                    </div>
+                    <div className="upload_search">
+                        <div><input className="search" type="text"
+                        placeholder="ㄴㅇ"/></div>
+                        <div>여기에 이제 리스트 쫙</div>
+                    </div>
+                </div>
                 <div className="upload_mid_bottom"></div>
             </div>
-            <div className="upload_right_section"></div>
+            <div className="upload_right_section">KUIZ 추천 포지션</div>
             <footer>
-                
+
             </footer>
         </div>
     );
