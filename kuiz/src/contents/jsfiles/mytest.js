49c459007e0474e7930006ac17231d133ed13e1d
jinsupark4255 <jin04007@naver.com>
2023-09-07 23:23:21 +0900

    나의 시험지 페이지 프로토타입 만들기 시작함

--- kuiz/src/contents/jsfiles/mytest.js
+++ kuiz/src/contents/jsfiles/mytest.js
@@ -5,7 +5,65 @@ export const MyTest = () => {
 
     return (
         <div className='mytest'>
-            
+            <div className='mytest_left'>
+                <div className='left_header'>
+                    <div className='left_header_top'>나의 시험지</div>
+                    <div className='left_header_bottom'>나의 시험지를 통해 틀린 문제를 점검하고 무엇무엇을 향상시켜보세용</div>
+                </div>
+                <div className='left_body'>
+                    <div className='left_body_header'>
+                        <div className='h1'>디렉토리명</div>
+                        <div className='h2'>강의명</div>
+                        <div className='h3'>테스트명</div>
+                        <div className='h4'>페이지</div>
+                        <div className='h5'>출제일</div>
+                        <div className='h6'>점수</div>
+                    </div>
+                    <div className='left_body_body'>
+                        <div className='left_body_contents'>
+                            <div className='b1'>fkfkfkfkfk</div>
+                            <div className='b2'>운체제</div>
+                            <div className='b3'>PDF 이름_번호</div>
+                            <div className='b4'>210</div>
+                            <div className='b5'>2023.01.02</div>
+                            <div className='b6'>13/20</div>
+                        </div>
+                        <div className='left_body_contents'>
+                            <div className='b1'>ㅁ낭러니ㅏㅇ리ㅏㄴㅇ러</div>
+                            <div className='b2'>ㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴ</div>
+                            <div className='b3'>ㄴㅇㄹㄴㅇㄹㅇㄹㄴ 이름_번호</div>
+                            <div className='b4'>2</div>
+                            <div className='b5'>2023.01.02</div>
+                            <div className='b6'>13/20</div>
+                        </div>
+                        <div className='left_body_contents'>
+                            <div className='b1'>디렉토리명</div>
+                            <div className='b2'>운영체제</div>
+                            <div className='b3'>PDF 이름_번호</div>
+                            <div className='b4'>23</div>
+                            <div className='b5'>2023.04.02</div>
+                            <div className='b6'>3/20</div>
+                        </div>
+                        <div className='left_body_contents'>
+                            <div className='b1'>디렉토리명</div>
+                            <div className='b2'>운영체제</div>
+                            <div className='b3'>PDF 이름_번호</div>
+                            <div className='b4'>23</div>
+                            <div className='b5'>2023.04.02</div>
+                            <div className='b6'>3/20</div>
+                        </div>
+                        <div className='left_body_contents'>
+                            <div className='b1'>디렉토리명</div>
+                            <div className='b2'>운영체제</div>
+                            <div className='b3'>PDF 이름_번호</div>
+                            <div className='b4'>23</div>
+                            <div className='b5'>2023.04.02</div>
+                            <div className='b6'>10/20</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='mytest_right'></div>
             <footer>
 
             </footer>
