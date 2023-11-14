e25dcce47a0724525065f6103a8a894145c27d75
최훈오 <gnsdh86166@gmail.com>
2023-11-14 23:12:27 +0900

    [Refactor] 문제풀이 footer 버튼 위치 조정

--- src/pages/TestList/styles/index.js
+++ src/pages/TestList/styles/index.js
@@ -202,6 +202,7 @@ export const AppContainer = styled.div`
 `;
 
 export const StickyFooter = styled.footer`
+	max-width: 1920px;
 	background-color: white;
 	box-shadow: 0px 4px 4px 4px rgba(0, 0, 0, 0.1);
 	width: 100%;
